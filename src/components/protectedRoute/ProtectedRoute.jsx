46adc2f6f44a69e4552817c10cff146af84b344f
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-10-09 17:01:50 +0300

    Bug Fixed

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,10 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import Loading from "./../loading/Loading";
+import { jwtDecode } from "jwt-decode";
 
 const ProtectedRoute = ({ children, requiredRole }) => {
-  const { user, token, loading } = useContext(AuthContext);
+  const { user, token, loading, logout } = useContext(AuthContext);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("zenfy-authToken");
+    if (storedToken) {
+      try {
+        const decoded = jwtDecode(storedToken);
+        const currentTime = Date.now() / 1000;
+
+        if (decoded.exp && decoded.exp < currentTime) {
+          logout();
+        }
+      } catch (err) {
+        console.error("Invalid token:", err);
+
+        logout();
+      }
+    }
+  }, [logout]);
 
   if (loading) {
     return <Loading />;
